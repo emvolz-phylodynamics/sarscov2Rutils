385bdcda47e794d96e20639038891cee44311e8b
emvolz <erik.volz@gmail.com>
2020-03-25 11:54:11 +0000

    adding daily inf plot, combining traj and log files

--- R/combineAndSummarize0.R
+++ R/combineAndSummarize0.R
@@ -81,6 +81,98 @@ combine_traj <- function(trajfns, burnProportion = .50, ntraj = 100, ofn = NULL 
 }
 
 
+#' Combine BEAST log AND trajectory files after removing burnin
+#'
+#" Additionally, if some BEAST runs converged to a different tree space with different posterior, will exclude log files from runs with significantly lower posterior (analysis of variance)
+#' The 'Sample' column is modified and shared between the combined log and trajectory files so that they can be referenced against eachother
+#'
+#" @param logfns Vector of log files 
+#" @param trajfns Vector of trajectory files. This MUST correspond to the same beast runs in logfns and be in the same order 
+#' @param bunrProportion Will remove this proportion from the beginning of logs
+#' @param ntraj This integer number of trajectories will be sampled from each trajectory file 
+#' @param ofn If not NULL will save the combined log to this file name 
+#' @param ofntraj If not NULL will save the combined traj to this file name 
+#' @return List with with combined logs and traj
+#' @export
+combine_logs_and_traj <- function(logfns, trajfns, burnProportion = .5 , ntraj = 200, ofn = NULL , ofntraj = NULL){
+	if ( length( logfns ) != length( trajfns))
+		stop('Provide *paired* log and traj files. These have different length.')
+	cat( 'These are the paired log and traj files provided\n')
+	print(cbind( logfns, trajfns ))
+	cat('****Make sure that these are paired correctly and each row corresponds to the same beast run****\n')
+	
+	Xs <- lapply( logfns, function(fn){
+		d = read.table( fn, header=TRUE, stringsAsFactors=FALSE) 
+		i <- floor( burnProportion * nrow(d))
+		tail( d, nrow(d) - i )
+	})
+
+	if ( length( Xs ) > 1 ){
+		medlogpos <- sapply( Xs, function(X) median (X$posterior ))
+		Xs <- Xs[ order( medlogpos, decreasing=TRUE ) ]
+		
+		xdf = data.frame(logpo = NULL, logfn = NULL ) 
+		k <- 0
+		for (X in Xs ){
+			k <- k + 1
+			xdf <- rbind ( xdf ,  data.frame( logpo = X$posterior , logfn = logfns[k] ) )
+		}
+
+		m <- aov(  logpo ~ logfn , data = xdf )
+		a <- TukeyHSD( m )
+		ps <- sapply( 1:(length(Xs)-1), function(k) {
+			a$logfn[ k, 4 ]
+		})
+		keep <- c( 1, 1 + which ( ps > .05 ) )
+		for ( k in keep )
+			Xs[[k]]$Sample <- paste(sep='.', Xs[[k]]$Sample, k )
+		Xs1 <- Xs[ keep ]
+		
+		
+		X <- do.call( rbind, Xs1)
+	} else{
+		X <- Xs[[1]]
+		keep <- 1
+	}
+	
+	# save comb log 	
+	if ( !is.null( ofn ))
+		saveRDS(X , file = ofn )
+	
+	cat('\n\n')
+	cat( 'EFFECTIVE SAMPLE SIZES OF COMBINED LOGS\n')
+	print( as.data.frame( unlist( sapply( o$log, function(x) if(is.numeric( x )) coda::effectiveSize( x ) ) ) ) )
+	cat('\n\n')
+	
+	# now combine traj 
+	Js <- lapply( keep , function(k){
+		fn = trajfns[k]
+		d = read.table( fn, header=TRUE, stringsAsFactors=FALSE)
+		ids = sort (unique( d$Sample ))
+		keepids <- tail(ids, floor(length(ids)-burnProportion*length(ids)))
+		keeptraj <- sample( keepids
+		 , size = min( length(keepids) , ntraj )
+		 , replace=FALSE)
+		d = d[ d$Sample %in% keeptraj , ]
+		d$Sample <- paste0( as.character(d$Sample) , '.', k)
+		d
+	})
+	if ( length( Js ) == 1)
+		J <- Js[[1]]
+	else 
+		J <- do.call( rbind, Js )
+	if ( !is.null ( ofntraj ))
+		saveRDS( J, file = ofntraj )
+	
+	cat( "These logs were retained:\n" )
+	print( logfns[keep] )
+	cat( "These traj files were retained:\n" )
+	print( trajfns[keep] )
+
+	list( log = X, traj = J  )
+}
+
+
 
 #' Compute R0, growth rate and doubling time for the SEIJR.0.0 model 
 #'
@@ -216,6 +308,111 @@ SEIJR_plot_size <- function(trajdf
 	  , I = I 
 	  , cases = cases 
 	  , exog = exog 
+	  , pldf =pldf 
+	  , case_data = case_data 
+	)
+}
+
+
+
+
+#' Plot the daily new infections through time from a SEIJR trajectory sample
+#' 
+#" Also computes CIs of various dynamic variables 
+#'
+#" @param trajdf Either a dataframe or a path to rds containing a data frame with a posterior sample of trajectories (see combine_traj)
+#' @param logdf Either a dataframe or a path to rds containing a data frame with posterior logs
+#' @param case_data An optional dataframe containing reported/confirmed cases to be plotted alongside estimates. *Must* contain columns 'Date' and 'Cumulative'. Ensure Date is not a factor or character (see as.Date )
+#' @param date_limits  a 2-vector containing bounds for the plotting window. If the upper bound is missing, will use the maximum time in the trajectories
+#' @param path_to_save Will save a png here 
+#' @return a list with derived outputs from the trajectories. The first element is a ggplot object if you want to further customize the figure 
+#' @export 
+SEIJR_plot_daily_inf <- function(trajdf
+  , logdf
+  , case_data = NULL
+  , date_limits = c( as.Date( '2020-02-01'), NA ) 
+  , path_to_save='daily.png'
+  , ...
+) {
+	library( ggplot2 ) 
+	library( lubridate )
+	
+	if (!is.data.frame(trajdf)){
+		# assume this is path to a rds 
+		readRDS(trajdf) -> trajdf 
+	}
+	
+	if (!is.data.frame( logdf ))
+		X <- readRDS(logdf )
+	
+	dfs <- split( trajdf, trajdf$Sample )
+	taxis <- dfs[[1]]$t 
+	
+	s <- sapply( dfs, function(df) df$Sample[1] )
+	X1 <- X[match( s, X$Sample ), ]
+	
+	if ( is.na( date_limits[2]) )
+		date_limits[2] <- as.Date( date_decimal( max(taxis)  ) )
+
+	qs <- c( .5, .025, .975 )
+
+	# infectious
+	Il = do.call( cbind, lapply( dfs, '[[', 'Il' ))
+	Ih = do.call( cbind, lapply( dfs, '[[', 'Ih' ))
+	I = Il + Ih 
+	t(apply( I, MAR=1, FUN= function(x) quantile(x, qs ))) -> Ici 
+	
+	# daily new inf 
+	Y = lapply( 1:length(dfs) , function(k) {
+		x = X1[k, ]
+		Il = dfs[[k]]$Il
+		Ih = dfs[[k]]$Ih
+		S = dfs[[k]]$S
+		E = dfs[[k]]$E
+		R = dfs[[k]]$R
+		tau = X1$seir.tau[k]
+		p_h = X1$seir.p_h[k] 
+		b = X1$seir.b[k]
+		y = (1/365) * ( b * Il + b * tau * Ih   ) *  S/ ( S + E + Il + Ih + R )
+	})
+	Y = do.call( cbind, Y )
+	Yci = t(apply( Y, MAR=1, FUN= function(x) quantile(na.omit(x), qs ))) 
+
+	#~ ------------
+	pldf <- data.frame( Date = ( date_decimal( taxis ) ) , reported=FALSE )
+	pldf$`New infections` = Yci[,1]
+	pldf$`2.5%` = Yci[,2]
+	pldf$`97.5%` = Yci[,3] 
+	
+	if ( !is.null( case_data )){
+		case_data$reported = TRUE
+		pldf <- merge( pldf, case_data , all = TRUE ) 
+	}
+	
+	pldf <- pldf[ with( pldf, Date > date_limits[1] & Date <= date_limits[2] ) , ]
+	pl = ggplot( pldf ) + 
+	  geom_path( aes(x = Date, y = `New infections` , group = !reported), lwd=1.25) + 
+	  geom_ribbon( aes(x = Date, ymin=`2.5%`, ymax=`97.5%`, group = !reported) , alpha = .25 ) 
+	
+	if ( !is.null(case_data) ) {
+		if( !(class(case_data$Date)=='Date') ){
+			stop('case_data Date variable must be class *Date*, not character, integer, or POSIXct. ') 
+		}
+		pl <- pl + geom_point( aes( x = Date, y = Confirmed ) ) 
+	}
+	pl <- pl + theme_minimal()  + xlab('') + 
+	 ylab ('Estimated daily new infections (ribbon) 
+	 Daily confirmed cases (points)' )  #+  scale_y_log10()
+	
+	if (!is.null(path_to_save))
+		ggsave(pl, file = path_to_save)
+	
+	list(
+	 pl
+	  , taxis = taxis 
+	  , Il = Il
+	  , Ih = Ih
+	  , Y
 	  , pldf =pldf 
 	  , case_data = case_data 
 	)
