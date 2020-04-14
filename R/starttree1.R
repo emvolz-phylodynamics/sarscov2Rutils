d270220d0675a4fe21c66133ef5451fc5a635dc1
David Jørgensen <36298489+JorgensenD@users.noreply.github.com>
2020-04-14 14:19:29 +0100

    call getRoot

--- R/starttree1.R
+++ R/starttree1.R
@@ -105,7 +105,7 @@ make_starting_trees <- function(  fastafn, treeoutfn='startTrees.nwk' , plotout=
   if(!is.null(plotout)){
     #trpl$edge.length <- pmax( 1e-6, trpl$edge.length / 29e3 )
     trroot <- tryCatch( { 
-		root(trpl, node=getRoot(tds[[1]]$intree)) # seems to raise error some times 
+		root(trpl, node=phangorn::getRoot(tds[[1]]$intree)) # seems to raise error some times 
 	} , error = function(e){
 		root(trpl, node=which.min(tds[[1]]$sts)  )
 	})
@@ -170,7 +170,7 @@ add_starting_trees_to_xml <- function( xmlfn ,  fastafn , plotout='MLtree.png', 
   if(!is.null(plotout)){
     #trpl$edge.length <- pmax( 1e-6, trpl$edge.length / 29e3 )
     trroot <- tryCatch( { 
-		root(trpl, node=getRoot(tds[[1]]$intree)) # seems to raise error some times 
+		root(trpl, node=phangorn::getRoot(tds[[1]]$intree)) # seems to raise error some times 
 	} , error = function(e){
 		root(trpl, node=which.min(tds[[1]]$sts)  )
 	})
