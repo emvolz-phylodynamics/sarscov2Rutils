11cca5eafe8a6efcefa168b674e0bde2360db106
emvolz <erik.volz@gmail.com>
2020-05-16 01:19:09 +0100

    skygrowth with covariates; reference sequences

--- R/skygrowth0.R
+++ R/skygrowth0.R
@@ -36,15 +36,16 @@ skygrowth0 <- function( tds , tstart = decimal_date( as.Date('2020-02-15') ), ta
 	tr <- tres[[1]] 
 	sts = sapply( strsplit( tr$tip, '\\|') , function(x) as.numeric( tail(x,2)[1] ) )
 	msts = max( sts ) 
+	mh = msts - tstart 
 	
 	.skygrowth <- function( tr ) {
 		tr = drop.tip( tr, tr$tip[ grepl( tr$tip, patt='_exog' ) ]  ) 
 		.res <- res
 		if ( is.null( res )) # guess a good res based on sample size 
-			.res <-  floor( 10 + 30*max(0, min( (Ntip(tr) - 50)/(300-50), 1 ) ) )
+			.res <-  floor( 10 + 20*max(0, min( (Ntip(tr) - 50)/(300-50), 1 ) ) )
 		
 		a = capture.output( { 
-				sg = skygrowth.mcmc( tr , res = .res , tau0 = tau0, logRmean = log(0.70), logRsd = .5, gamma = gamma1, ... )# , mhsteps = 1e6)
+				sg = skygrowth.mcmc( tr , res = .res , tau0 = tau0, logRmean = log(0.70), logRsd = .5, gamma = gamma1, maxHeight = mh, ... )# , mhsteps = 1e6)
 		}) 
 		sg
 	}
@@ -91,6 +92,133 @@ skygrowth0 <- function( tds , tstart = decimal_date( as.Date('2020-02-15') ), ta
 	, class = 'sarscov2skygrowth' 
 	)
 }
+
+
+
+
+#' Compute skygrowth Ne(t) and R(t) estimates for a given list of time scaled trees
+#'
+#" Fits to covariate data 
+#' 
+#' If tstart is provided than you can supply a list of ape::phylo trees instead of treedater trees 
+#' 
+#' @param tds A list or multiPhylo, containing ape::phylo or treedater trees
+#' @param X A data frame with covariate data, must include numeric column 'time'
+#' @param formula Specify the relationship between Change in growth rate and covariates. Should have empty lhs and only specify rhs 
+#' @param tstart A numeric time to consider as the beginning of the epidemic in region; for best results, select a time when exponential growth in region is well underway. Can be NULL in which case the sarscov2::timports function is used to guess an appropriate date
+#' @param tau0 Precision parameter for skygrowth. Default value corresponds to 1 per cent change in growth per week 
+#' @param see skygrowth, if omitted will guess a good value
+#' @param  numpb Number of parametric bootstrap to use if using timports
+#' @param ncpu Number CPUs to use 
+#' @param gamma1 Death/recovery rates used for translating growth rates into R(t) 
+#' @param ... Additional arguments passed to skygrowth (eg mhsteps)
+#' @return A list with dataframes for Ne, growth, and R(t)
+#' @examples
+#' \dontrun{
+#' library( ape ) ; library( sarscov2 )
+#' tres = read.tree( 'startTrees.nwk' ) 
+#' o = skygrowth0 ( tres, start = decimal_date( as.Date('2020-02-15') ) )
+#' }
+#' @export 
+skygrowth1 <- function( tds 
+ , X = NULL
+ , formula = ~ transit_stations_percent_change_from_baseline
+ , tstart = decimal_date( as.Date('2020-03-01') )
+ , tau0 = 1/365 / 36.5^2, res = NULL,  numpb = 10, ncpu = 6, gamma1 = (-log(.5)) * 365 / 6.5
+ , ...
+){
+	stopifnot( require( skygrowth )  )
+	library( lubridate )
+	
+	if ( is.null( tstart )){
+		tis = compute_timports( tds , numpb = numpb , ncpu = ncpu )
+		timport_quantile = .25
+		tstart <- quantile( tis[, 'y' ] , timport_quantile )
+	}
+	if ( !is.numeric( tstart ))
+		tstart <- decimal_date( tstart )
+	
+	tres = lapply( tds, function(x) { class(x)='phylo'; x } )
+	
+	tr <- tres[[1]] 
+	sts = sapply( strsplit( tr$tip, '\\|') , function(x) as.numeric( tail(x,2)[1] ) )
+	msts = max( sts ) 
+	mh = msts - tstart 
+	
+	.skygrowth <- function( tr ) {
+		tr = drop.tip( tr, tr$tip[ grepl( tr$tip, patt='_exog' ) ]  ) 
+		.res <- res
+		if ( is.null( res )) # guess a good res based on sample size 
+			.res <-  floor( 10 + 20*max(0, min( (Ntip(tr) - 50)/(300-50), 1 ) ) )
+		if ( !is.null(X)){
+			a = capture.output( { 					
+					sg = skygrowth.mcmc.covar(tr
+					   , ~ transit_stations_percent_change_from_baseline
+					   , data =  X
+					   , maxSample= msts
+					   , tau0 = tau0
+					   , res = .res
+					   , maxHeight = mh
+					   , gamma = gamma1
+					   ,  logRmean = log(0.70), logRsd = .5
+					   , ...
+					)
+			})  
+		} else{
+			a = capture.output( { 
+					sg = skygrowth.mcmc( tr , res = .res , tau0 = tau0, logRmean = log(0.70), logRsd = .5, gamma = gamma1, maxHeight = mh, ... )# , mhsteps = 1e6)
+			}) 
+		}
+		sg
+	}
+	
+	
+	sgs = parallel::mclapply( tres, .skygrowth, mc.cores = ncpu  )
+	taxis = seq( tstart, msts , length =  floor((msts - tstart)*365) )
+	dates = date_decimal( taxis )
+	
+	# Ne
+	Ne <- do.call( cbind, lapply( sgs , function(sg){
+		apply( sg$ne, MAR=1, FUN= function(x) approx( sg$time + msts, x , rule=2, xout = taxis)$y )
+	}))
+	NeCI = t( apply( Ne, MAR=1, FUN = function(x) quantile(x, c(.025, .5, .975 )) ) )
+	colnames(NeCI)= c( 'pc2.5', 'pc50', 'pc97.5' ) 
+	NeCI = data.frame( time = dates, NeCI )
+	
+	# growth rate 
+	GR <- do.call( cbind, lapply( sgs , function(sg){
+		apply( sg$growthrate, MAR=1, FUN= function(x) approx( sg$time+msts, x , rule=2, xout = taxis)$y )
+	}))
+	GRCI = t( apply( GR, MAR=1, FUN = function(x) quantile(x, c(.025, .5, .975 )) ) )
+	colnames(GRCI)= c( 'pc2.5', 'pc50', 'pc97.5' ) 
+	GRCI = data.frame( time = dates, GRCI )
+	
+	Tg = 1/gamma1
+	# growthrate = (R0-1)/Tg
+	Rmat <-  GR*Tg + 1
+	Rmat[ Rmat < 0 ] <- 0#NA 
+	RCI =  t( apply( Rmat, MAR=1, FUN = function(x) quantile(na.omit(x), c(.025, .5, .975 )) ) )
+	colnames(RCI) = c( 'pc2.5', 'pc50', 'pc97.5' ) 
+	RCI = data.frame( time = dates, RCI )
+	
+	structure(
+	list(
+	  taxis = taxis
+	  , date = dates
+	  , Ne = NeCI
+	  , growth= GRCI
+	  , R =RCI
+	  , GRmat = GR 
+	  , Rmat = Rmat 
+	  , beta = lapply( sgs, '[[', 'beta' )
+	)
+	, class = 'sarscov2skygrowth' 
+	)
+}
+
+
+
+
 
 #todo 
 .phylodyn <- function( tr ) {
